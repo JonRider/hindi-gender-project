b3bd0f2c757260d51d7fb0efbe33bedd9a3a2ee1
JonRider <jon@rider.net>
2019-09-27 18:26:12 +0530

    Styling

--- gender/views.py
+++ gender/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import DB models
+from .models import Noun, Marker
+
+# Views
+def index(request):
+
+    # Access our nouns model
+    nouns = Noun.objects.all()
+
+    context = {
+        "message": "Hello Everyone!!!!",
+        "nouns": nouns
+    }
+
+    return render(request, "gender/index.html", context)
