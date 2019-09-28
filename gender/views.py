171f22bea9eb5287033d5a7cb6ab70c4fd5784a1
JonRider <jon@rider.net>
2019-09-28 14:59:41 +0530

    Frontend and Backend Login

--- gender/views.py
+++ gender/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as dj_login, logout
+from django.contrib.auth.models import User
 
 # Import DB models
 from .models import Noun, Marker
@@ -15,3 +17,17 @@ def index(request):
     }
 
     return render(request, "gender/index.html", context)
+
+def login(request):
+    if request.method == "GET":
+        return render(request, "gender/login.html", {"message": ""})
+    else:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            dj_login(request, user)
+            # Eventually this will redirect to a new page
+            return render(request, "gender/login.html", {"message": "Logged in Successfully"})
+        else:
+            return render(request, "gender/login.html", {"message": "Invalid credentials."})
